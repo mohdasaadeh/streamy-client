20bb7d3d95e0dba353674bb64dee3bff2414c6dd
mohdasaadeh <mass.1993@hotmail.com>
2022-04-19 16:16:28 +0300

    Connecting the app with the RTMP server

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -33,10 +33,10 @@ class StreamList extends React.Component {
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
-          <div className="content">
+          <Link to={`/streams/show/${stream.id}`} className="content">
             {stream.title}
             <div className="description">{stream.description}</div>
-          </div>
+          </Link>
         </div>
       );
     });
--- src/components/streams/StreamShow.js
+++ src/components/streams/StreamShow.js
@@ -1,7 +1,54 @@
 import React from "react";
+import { connect } from "react-redux";
+import flvjs from "flv.js";
 
-const StreamShow = () => {
-  return <div>StreamShow</div>;
+import { fetchStream } from "../../actions";
+
+class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+
+    if (this.props.stream) {
+      if (flvjs.isSupported()) {
+        this.flvPlayer = flvjs.createPlayer({
+          type: "flv",
+          url: `http://localhost:8000/live/${id}.flv`,
+        });
+        this.flvPlayer.attachMediaElement(this.videoRef.current);
+        this.flvPlayer.load();
+      }
+    }
+  }
+
+  componentWillUnmount() {
+    this.flvPlayer.destroy();
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <h1>{this.props.stream.title}</h1>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls />
+        <h5>{this.props.stream.description}</h5>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamShow;
+export default connect(mapStateToProps, { fetchStream })(StreamShow);
